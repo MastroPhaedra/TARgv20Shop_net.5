9ca7495ac93d5907da3a0d6b52319037881047c1
Ingvar Derešivski <inde999@gmail.com>
2021-11-08 14:12:37 +0200

    ProductAndDbContext

---
+++ Targv20Shop/Targv20Shop.Data/Targv20ShopDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targv20Shop.Data
+{
+    public class Targv20ShopDbContext : DbContext
+    {
+        public Targv20ShopDbContext(DbContextOptions<Targv20ShopDbContext> options)
+            : base(options) { }
+
+    }
+}
